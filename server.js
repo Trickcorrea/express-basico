70bc63602bca134013a4130ac4ce98c32e3cf21f
Patrick Correa <kakike15@yahoo.com.br>
2019-03-22 01:22:25 -0300

    melhorando com process env port e cb server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var app = express();
-
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 app.get('/', function(req, res){
     res.send('Ola Mundo Novamente agora com git, vamos usar mais um git');
 });
 
-app.listen(3000);
+app.listen(port, function(){
+    console.log('Server run: http://localhost:'+port)
+});
